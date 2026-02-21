9c2c1b31c9755f22996de5a156ad08336e690b8d
Arely Tellez <tellezarely2018@gmail.com>
2026-02-21 11:23:42 -0600

    Favicon agregado

--- js/game.js
+++ js/game.js
@@ -763,4 +763,10 @@ startGameBtn.addEventListener("click", function(){
   },300);
 
 });
+const cursor = document.querySelector(".custom-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
 
